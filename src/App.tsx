2b182278c851d02f67a7df749962b9a8d4255e59
Michael <mtreser@rekor.ai>
2023-05-18 23:12:28 +0300

    chore(routing): Added routing. Code splitting with React lazy & suspense.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import SpButton from "./components/ui/sp-button/spButton";
 import './styles.scss';
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div>
+            <div className="navbar">
+                <Link className='nav-link' to={'/'}>Home</Link>
+                <Link className='nav-link' to={'/about'}>About</Link>
+            </div>
             React App
+            <Suspense fallback={<div>Loading... </div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>} />
+                    <Route path={'/about'} element={<AboutPageAsync/>} />
+                </Routes>
+            </Suspense>
             <SpButton />
         </div>
     );
