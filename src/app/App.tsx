cb6538e9e026ef495bf9fda1479b99ead3e11a07
ArMikael <armikael2@gmail.com>
2023-05-21 17:37:56 +0300

    chore(routes): Routes moved to app/providers. Added routeConfig to shared/config.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import SpButton from "../components/ui/sp-button/spButton";
 import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from "pages/AboutPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -16,14 +15,8 @@ const App = () => {
                 <Link className='nav-link' to={'/'}>Home</Link>
                 <Link className='nav-link' to={'/about'}>About</Link>
             </div>
-            React App
-            <Suspense fallback={<div>Loading... </div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage />} />
-                    <Route path={'/about'} element={<AboutPage />} />
-                </Routes>
-            </Suspense>
 
+            <AppRouter />
             <button onClick={toggleTheme}>Change Theme</button>
             <SpButton />
         </div>
